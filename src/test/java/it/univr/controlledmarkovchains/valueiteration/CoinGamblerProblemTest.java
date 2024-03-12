c3311630d6967fced77b2a1b358bb798fa507d60
Andrea Mazzon <mazzon@math.lmu.de>
2024-03-12 09:12:26 +0100

    Added dynamic plot

--- src/test/java/it/univr/controlledmarkovchains/valueiteration/CoinGamblerProblemTest.java
+++ src/test/java/it/univr/controlledmarkovchains/valueiteration/CoinGamblerProblemTest.java
@@ -1,11 +1,15 @@
 package it.univr.controlledmarkovchains.valueiteration;
 
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.DoubleUnaryOperator;
 
 import net.finmath.plots.Named;
 import net.finmath.plots.Plot2D;
+import net.finmath.plots.Plotable2D;
+import net.finmath.plots.PlotableFunction2D;
 
 /**
  * This class tests the implementation of ValueIteration and its derived class CoinGamblerProblem.
@@ -15,10 +19,9 @@ import net.finmath.plots.Plot2D;
  */
 public class CoinGamblerProblemTest {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
-		
-		double requiredPrecision = 1e-9;
+		double requiredPrecision = 1e-4;
 
 		double headProbability = 0.4;
 		
@@ -28,6 +31,7 @@ public class CoinGamblerProblemTest {
 		
 		ValueIteration problemSolver = new CoinGamblerProblem(discountFactor, requiredPrecision, headProbability, moneyToWin);
 
+		//plot of the last value functions
 		double[] valueFunctions = problemSolver.getValueFunctions();
 		
 		final Plot2D plotValueFunctions = new Plot2D(1, moneyToWin-1, moneyToWin-1, Arrays.asList(
@@ -38,6 +42,7 @@ public class CoinGamblerProblemTest {
 		
 		plotValueFunctions.show();
 		
+		//plot of the last optimal actions
 		double[] optimalActions = problemSolver.getOptimalActions();
 			
 		
@@ -49,6 +54,34 @@ public class CoinGamblerProblemTest {
 		
 		plotOptimalPolicy.show();
 		
-	}
+		//dynamic plot of the updated value functions
+		
+		//we start from the very first ones..
+		ArrayList<double[]> updatedValues = problemSolver.getUpdatedOptimalValues();
+		
+		double[] firstValueFunctions = updatedValues.get(0);//note the get(int index) method of ArrayList
+		
+		final Plot2D plotUpdatedValueFunctions = new Plot2D(1, moneyToWin-1, moneyToWin-1, Arrays.asList(
+				new Named<DoubleUnaryOperator>("Updated value function", x -> firstValueFunctions[(int) x])));
 
+		plotUpdatedValueFunctions.setXAxisLabel("State");
+		plotUpdatedValueFunctions.setYAxisLabel("Updated value function, iteration " + 1);
+		plotUpdatedValueFunctions.show();
+		Thread.sleep(1000);
+
+		//..and then we dynamically print the next ones, until the final ones
+		for (int iterationIndex = 1; iterationIndex < updatedValues.size(); iterationIndex ++) {
+			
+			//this is what we want to plot, at every iteration of the while loop
+			double[] updatedValueFunction = updatedValues.get(iterationIndex);
+			
+			final List<Plotable2D> plotables = Arrays.asList(
+					new PlotableFunction2D(1, moneyToWin-1, moneyToWin-1,
+							new Named<DoubleUnaryOperator>("Updated value function", x -> updatedValueFunction[(int) x]),null));
+
+			plotUpdatedValueFunctions.update(plotables);//in this way, all the plots are in the same figure
+			plotUpdatedValueFunctions.setYAxisLabel("Updated value function, iteration " + (iterationIndex+1));
+			Thread.sleep(1000); 
+		}
+	}
 }
